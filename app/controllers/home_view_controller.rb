0c7b9f2c6b9eac70d32b7786329f587b64a82e84
Raymond T. Hightower <rth@wisdomgroup.com>
2013-10-24 15:26:40 -0500

    add a subview

--- app/controllers/home_view_controller.rb
+++ app/controllers/home_view_controller.rb
@@ -8,6 +8,16 @@ class HomeViewController < UIViewController
 
     3.times do |i|
       subframe = scroll_frame.right(scroll_frame.width * 1)
+      subview = UIView.alloc.initWithFrame(subframe)
+      subview.backgroundColor = [85,99,0].uicolor
+      scroll << subview
+
+      header_frame = subview.bounds.down(30).right(30).width(150).height(20)
+      header_label.text = "This is page #{i + 1}."
+      header_label.backgroundcolor = :clear.uicolor
+      subview << header_label
+
+
 
     end
   end
